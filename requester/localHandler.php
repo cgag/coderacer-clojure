a428d2f04f518a39f9c11387abd1fcd9cb8e83e5
Caleb Hyde <caleb.hyde@gmail.com>
2012-06-03 04:30:28 -0500

    Parse multiple categories and write to sub-folders.

--- requester/localHandler.php
+++ requester/localHandler.php
@@ -1,5 +1,7 @@
 <?php
 
+$dataDir = 'data/';
+
 function parseCategories($inFile, $outFile) {
   $response = file_get_contents($inFile);
   $response = json_decode($response);
@@ -8,16 +10,13 @@ function parseCategories($inFile, $outFile) {
   $sink = fopen($outFile, 'w');
   foreach ($cats as $cat) {
     $i = $cat->title;
-    // attempt to sanitize filenames. Consider using md5() instead.
-    $i = preg_replace('/\s/', '_', $i);
-    $i = preg_replace('/\//', '-', $i);
-    $i = preg_replace('/\"/', '+', $i);
-    fwrite($sink, "$i\n");
+    fwrite($sink, $i . "\n");
   }
   fclose($sink);
 }
 
-function parseRevisions($inFile) {
+function parseRevisions($inFile, $outPath) {
+  echo "Parsing $inFile\n";
   // Begin: terrible-hack to handle Mediawiki's json tree. There must be a better way... but not before 11am!
   $loaded = file_get_contents($inFile);
   $response = json_decode($loaded);
@@ -32,10 +31,24 @@ function parseRevisions($inFile) {
   foreach ($results[0] as $lang) {
     $result = preg_match('/\<lang\s*(\w*)\>(.*?)\<\/lang\>/s', $lang, $code);
     if ( sizeof($code) )
-      file_put_contents("data/$code[1]", $code[2]); // write each bare language example to a file
+      if (! is_dir($outPath) ) {
+	mkdir($outPath);
+	echo $code[1] . "\n";
+      }
+    file_put_contents("$outPath/$code[1]", $code[2]); // write each bare language example to a file
   }
 }
-parseCategories('data/problems.json', 'data/problems.txt');
-//  parseRevisions('data/revisions.json');
 
+function parseAllRevisions($dataDir) {
+  $files = scandir($dataDir);
+  foreach ($files as $file) {
+    $pathParts = pathinfo($file);
+    if ($pathParts['extension'] == 'json') {
+      parseRevisions($dataDir . $file, $dataDir . "parsed/" . $pathParts['filename']);
+    }
+  }
+}
+
+parseCategories('data/problems.json', 'data/problems.txt');
+  parseAllRevisions($dataDir);
 ?>
--- requester/reqHttp.php
+++ requester/reqHttp.php
@@ -3,30 +3,15 @@ require_once "HTTP/Request.php";
 
 $dataDir = 'data/';
 $revisionsStore = $dataDir . 'revisions.json';
-$catStore = $dataDir . 'problems.json';
-
+$catStore = $dataDir . 'problems.json';  
 $apiBase = 'http://rosettacode.org/mw/api.php?';
-$queryRevisions = 
-  array(
-	'action' => 'query',
-	'prop' => 'revisions',
-	'rvprop' => 'content',
-	'rvlimit' => '1',
-	'rvdir' => 'older',
-	'generator' => 'search',
-	'gsrsearch' => 'Apply a callback to an array',
-	'gsrlimit' => '1',
-	'rvexpandtemplates' => '1',
-	//	'rvparse' => '1',
-	'format' => 'json'
-	);
 
 $queryCats =
   array(
 	'action' => 'query',
 	'list' => 'categorymembers',
 	'cmtitle' => 'Category:Ada',
-	'cmlimit' => '100',
+	'cmlimit' => '7',
 	'format' => 'json'
 	);
 
@@ -45,7 +30,50 @@ function makeRequest($base, $params, $localStore) {
 
   file_put_contents($localStore, $response);
 }
- makeRequest($apiBase, $queryCats, $catsStore);
- makeRequest($apiBase, $queryRevisions, $revisionsStore);
+
+function requestAllCategories() {
+
+}
+
+function requestAllRevisions() {
+  $apiBase = 'http://rosettacode.org/mw/api.php?';
+  $dataDir = 'data/';
+  $revisionsStore = $dataDir . 'revisions.json';
+  $catNames = $dataDir . 'problems.txt';
+
+  $queryRevisions = 
+    array(
+	  'action' => 'query',
+	  'prop' => 'revisions',
+	  'rvprop' => 'content',
+	  'rvlimit' => '1',
+	  'rvdir' => 'older',
+	  'generator' => 'search',
+	  // 'gsrsearch' => 'Apply a callback to an array',
+	  'gsrlimit' => '1',
+	  'rvexpandtemplates' => '1',
+	  //	'rvparse' => '1',
+	  'format' => 'json'
+	  );
+
+  $cats = file($catNames);
+  foreach( $cats as $catIdx => $cat ) {
+    echo "Querying for $cat";
+    $queryRevs = array_merge($queryRevisions, array('gsrsearch' => "$cat"));
+
+    // attempt to sanitize filenames. Consider using md5() instead.
+    $cat = preg_replace('/\s/', '_', trim($cat));
+    $cat = preg_replace('/\//', '-', $cat);
+    $cat = preg_replace('/\"/', '+', $cat);
+    $catStore = $dataDir . "raw/" . "$cat" . ".json";
+    // echo " would write to $catStore \n";
+    makeRequest($apiBase, $queryRevs, $catStore);
+  }
+
+  //  makeRequest($apiBase, $queryRevisions, $revisionsStore);
+}
+
+ makeRequest($apiBase, $queryCats, $catStore);
+ requestAllRevisions();
 
 ?>
