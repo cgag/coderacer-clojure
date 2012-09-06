e82ce960e648d1e2548b1d3efa90b03f35dafba4
Curtis Gagliardi <gagliardi.curtis@gmail.com>
2012-09-05 20:57:30 -0500

    experiment with agent based aleph responder

--- clj/aleph-testing/src/aleph_testing/core.clj
+++ clj/aleph-testing/src/aleph_testing/core.clj
@@ -2,14 +2,24 @@
   (:use [lamina.core]
         [aleph.http]))
 
+(def a (agent 0))
+
+(defn respond [_ ch]
+  (Thread/sleep 4000)
+  (enqueue ch {:status 200
+              :headers {"content-type" "text/html"}
+              :body (.getName (Thread/currentThread))}))
+
 (defn hello-world [channel request]
   (future
     (Thread/sleep 5000)
     (enqueue channel
              {:status 200
               :headers {"content-type" "text/html"}
-              :body "With future"})))
+              :body (.getName (Thread/currentThread))})))
 
+(defn agent-hello-world [channel request]
+  (send a respond channel))
 
 (defn sync-hello-world [request]
   (Thread/sleep 5000)
@@ -21,8 +31,8 @@
 (defn stream-numbers [ch]
   (future
     (dotimes [i 5]
-      (dotimes [i 3000]
-        (enqueue ch (str i)))
+      (dotimes [j 3000]
+        (enqueue ch (str j)))
       (Thread/sleep 2000))
     (close ch)))
 
@@ -36,7 +46,7 @@
 (def wrapped-handler (wrap-ring-handler handler))
 (def wrapped-sync (wrap-ring-handler sync-hello-world))
 
-(def server (start-http-server #'hello-world {:port 8080}))
+(def server (start-http-server #'agent-hello-world {:port 8080}))
 
 (defn stop-server [server-fn]
   (server-fn))
