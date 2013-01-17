cbf78e55c4e7f7a9ee16f6317f3887b9fc81fa5c
Curtis Gagliardi <gagliardi.curtis@gmail.com>
2013-01-17 13:52:11 -0600

    Move aleph testing stuff to its own project

--- clj/aleph-testing/project.clj
+++
@@ -1,7 +0,0 @@
-(defproject aleph-testing "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [aleph "0.3.0-alpha2"]])
